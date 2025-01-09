d30504396d9e32be1c73781e3057228794e1ded7
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-01-09 13:26:33 +0200

    fix bags

--- script.js/DeathLevel.js/backgroundAudio.js
+++ script.js/DeathLevel.js/backgroundAudio.js
@@ -1,9 +1,24 @@
-let bgSound = document.createElement("audio");
-bgSound.src = "./audio/DeathLevel/backgroundAudio.mp3";
+ let bgSound = document.createElement("audio");
+ bgSound.src = "./audio/DeathLevel/backgroundAudio.mp3";
 
-export function backgroundAudio(){
-    bgSound.play();
-    bgSound.volume = 0.10;
-    bgSound.loop = true;
-}
-backgroundAudio();
+ function backgroundAudio(){
+     bgSound.volume = 0.10;
+     bgSound.loop = true;
+     bgSound.play();
+ }
+ 
+const message = document.createElement("div");
+message.innerText = "Кликните чтобы включить музыку!";
+message.style.position = "fixed";
+message.style.top = "10px";
+message.style.left = "30px";
+message.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+message.style.color = "white";
+message.style.padding = "20px";
+message.style.borderRadius = "10px";
+document.body.appendChild(message);
+
+message.addEventListener("click", () => {
+    backgroundAudio();
+    message.remove();
+}, { once: true });
--- script.js/DeathLevel.js/soundOfShot.js
+++ script.js/DeathLevel.js/soundOfShot.js
@@ -2,6 +2,7 @@ let audioShot = document.createElement("audio");
 audioShot.src = "./audio/DeathLevel/shot.mp3";
 
 export function soundOfShot(){
+    audioShot.currentTime = 0.1;
     audioShot.play();
     audioShot.valume = 0.20;
 }
