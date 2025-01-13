a8d3db792969bbedf20fa997bd2c50e7a9fe4e18
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-01-13 22:43:19 +0200

    fix healthPlayer

--- script.js/DeathLevel.js/healthPlayer.js
+++ script.js/DeathLevel.js/healthPlayer.js
@@ -3,7 +3,7 @@ import { staminaItem, setValue, getValue, staminaCounter } from "./stamina.js";
 
     export function heal(){
     document.addEventListener("keydown",(event)=>{
-        if(event.key === "h"){
+        if(event.code === "KeyH"){
             if(staminaCounter >= 100){
                 MainPlayer.hpPlayer = 100;
                 MainPlayer.hpBarPlayer.style.width = MainPlayer.hpPlayer + "%";
