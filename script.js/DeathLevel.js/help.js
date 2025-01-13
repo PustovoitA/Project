e1ca5799cf9c785f59e380591f9a338270e5e6cc
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-01-13 22:48:14 +0200

    fix help

--- script.js/DeathLevel.js/help.js
+++ script.js/DeathLevel.js/help.js
@@ -1,6 +1,6 @@
 
 
-let helpItem = document.createElement("p");
+export let helpItem = document.createElement("div");
 export function help(){
     helpItem.textContent = "Heal - H";
     document.querySelector(".help").append(helpItem);
